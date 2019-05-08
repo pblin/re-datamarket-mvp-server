41a998bb168caf85a6a2a7917785c560e2ea6d74
Lin, Bernard <bernardlin@outlook.com>
2019-05-08 02:51:45 -0400

    fixed email confirmation/verification api

--- api-server/src/routes/profile/profile.controller.ts
+++ api-server/src/routes/profile/profile.controller.ts
@@ -17,14 +17,14 @@ router.get('/:email', (req, res, next) => {
                 else
                     res.sendStatus(404);
             }
-        }).catch(() => {
+        }).catch((result) => {
             return res.sendStatus(500); //TODO: Introduce better error handling
             });
 });
 
 router.get('/verify/:email/:code', (req, res) => {
-    if (req.params.email == null ||req.params.code == null ) {
-        return false;
+    if (req.params.email == null || req.params.code == null ) {
+        return res.sendStatus(404);
     }
     // console.log(req.params.email);
     // console.log(req.params.code);
@@ -34,15 +34,41 @@ router.get('/verify/:email/:code', (req, res) => {
              return res.status(500).send(false);
     });
 });
+
+router.post('/confirm/:which', (req, res) => {
+    if (req.params.which) {
+        return res.sendStatus(404);
+    }
+    let profile:ProfileData = req.body; 
+    let email;
+    if (req.params.which == 2) 
+        email = profile.secondary_email; 
+    else 
+        email = profile.primary_email;
+
+    
+});
 router.post('/', (req, res) => {
-    return profileService.upsertProfile(req.body).then((result) => {
-            if (result != null) 
-                res.send(result);
+    let email = null;
+    let profile:ProfileData = req.body;
+
+    if (req.query.confirmEmail != undefined)
+        if (req.query.confirmEmail=='2') 
+            email = profile.secondary_email;
+        else
+            email = profile.primary_email;
+
+    console.log ("email=" + email);
+    return profileService.sendVerification(email,profile).then((profile) => {
+                return profileService.upsertProfile(profile);
+         }).then ((result) => {
+            if (result != null)
+                res.send(200);
             else 
-                res.sendStatus(404);
+                res.status(500).send("profile update error");
         }).catch((err) => {
-             return res.sendStatus(500);
-         });
+            return res.status(500).send("service error");
+    });
 });
 
 export default router;
