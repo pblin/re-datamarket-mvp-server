fdabb383e1a72a229344ca42e65edd7994441dcc
Lin, Bernard <bernardlin@outlook.com>
2019-05-13 17:33:17 -0400

    revert This reverts commit 3c101e6367726920b9f2c2544425c0e9a92ea674.

--- rebloc-blockchain/migrations/2_deploy_contract.js
+++
@@ -1,9 +0,0 @@
-var KittyMinting = artifacts.require("KittyMinting");
-var KittyBreeding= artifacts.require("KittyBreeding");
-var KittyOwnership = artifacts.require("KittyOwnership");
-
-module.exports = function(deployer) {
-    deployer.deploy(KittyMinting);
-    deployer.deploy(KittyBreeding);
-    deployer.deploy(KittyOwnership);
-};
