e57f015101df7e11bf8b4fd34e7c3ff60d27eb50
Lin, Bernard <bernardlin@outlook.com>
2022-05-08 18:29:31 -0400

    another try

--- api-server/src/routes/stripe/stripe.controller.ts
+++ api-server/src/routes/stripe/stripe.controller.ts
@@ -2,16 +2,20 @@ import * as express from 'express';
 import { StripeServices } from './stripe.service';
 import { LogService } from '../../utils/logger';
 const logger = new LogService().getLogger();
-const router = express.Router();
+var router = express.Router();
 // var AsyncRouter = require("express-async-router").AsyncRouter;
 // var router = AsyncRouter();
 
-const stripeServices = new StripeServices();
+router.get('/ping',  (req, res) => {
+    res.status(200).send('i am herer!')
+});
+
 router.post('/charge/:userid', (req, res) => {
     console.log('CC charge...');
     logger.info(JSON.stringify(req.body));
     if (req.params.userid === undefined) 
       res.sendStatus(404).send("user id needed");
+    var stripeServices = new StripeServices();
     return stripeServices.processCharge(req.params.userid, req.body).then ( (result) => {
       logger.info(result);
       if (result['status']['ref'] != "ok")
